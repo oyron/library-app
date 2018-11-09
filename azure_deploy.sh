42d013180e06d0b36a346da56faea09401953313
oyron <oyron@statoil.com>
2018-11-09 23:46:08 +0100

    Deploy script updates

--- azure_deploy.sh
+++ azure_deploy.sh
@@ -3,6 +3,7 @@
 set -e
 
 az account set --subscription "VanDamme"
-az webapp create --resource-group DataPlatformOYRONRGDev --plan LibrarySP --name library-web --runtime "NODE|10.1" --deployment-local-git
+az appservice plan create --name LibraryWebSP --resource-group DataPlatformOYRONRGDev --sku B1 --is-linux --location "North Europe"
+az webapp create --resource-group DataPlatformOYRONRGDev --plan LibraryWebSP --name library-web --runtime "NODE|10.10" --deployment-local-git
 git remote add azure https://oyron@library-web.scm.azurewebsites.net:443/library-web.git
 git push azure master
